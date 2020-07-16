20e6216f592216f2e1b1a4fe22285f91b3181436
Zach Zukowski <zzukowski@ejfcap.com>
2020-07-15 21:08:11 -0400

    Updates

---
+++ precache-manifest.d3827e5e5c3acf90a18983991b3051c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78220f20f27399f47e7bd36f68a90f7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c86d0370dd1ffe1b68c",
+    "url": "/portfolio/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "35293ad3f045b1e08dac",
+    "url": "/portfolio/static/css/main.94c8d3ce.chunk.css"
+  },
+  {
+    "revision": "7c86d0370dd1ffe1b68c",
+    "url": "/portfolio/static/js/2.41ce971a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.41ce971a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35293ad3f045b1e08dac",
+    "url": "/portfolio/static/js/main.be9ad305.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.99eadd4d1dcb3ebb086758cf19194e48.js"
+  "/portfolio/precache-manifest.d3827e5e5c3acf90a18983991b3051c4.js"
 );
 
 self.addEventListener('message', (event) => {
